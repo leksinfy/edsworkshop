6a6b767d058fd1bcfd2540baaf3989f05b37aadf
leksinfy <inf45553@adobe.com>
2026-02-17 21:48:28 +0530

    Update cards.js

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -23,11 +23,4 @@ export default function decorate(block) {
   block.append(ul);
 }
 
-export default function decorate(element, fieldJson, container, formId) {
-    element.classList.add('card');
-    element.querySelectorAll('.radio-wrapper').forEach((radioWrapper) => {
-        const image = createOptimizedPicture('https://main--afb--jalagari.hlx.live/lab/images/card.png', 'card-image');
-        radioWrapper.appendChild(image);
-    });
-    return element;
-}
+
